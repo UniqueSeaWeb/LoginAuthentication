594b8b464b25dcb9df59519b791bf940d9441bd6
UniqueSea <uniquesea912@gmail.com>
2024-04-23 23:05:40 +0900

    login.html

--- loginForm_files/css/login.js
+++ loginForm_files/css/login.js
@@ -1,1 +1,11 @@
 /* ログイン */
+async function doGet() {
+    try {
+        const WebAppUrl = 'https://script.google.com/macros/s/AKfycbwuY3rui2stn9OvG4aVYSuZqQMQYa9TrAfHVJX1byXFSfhwWU3uclpYFQ37jsPzpdM/exec';
+        const response = await fetch(WebAppUrl + '?userId=' + userId + '&password=' + password);
+        const data = await response.json();
+        // Process data
+    } catch (error) {
+        // Handle errors
+    }
+}
